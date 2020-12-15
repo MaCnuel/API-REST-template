270655efc3e4c5f8dd44eed463e560a30eb08255
Manuel Penya <manuel.penya.trabajo@gmail.com>
2020-12-15 10:08:12 +0100

    Mistake npm i axios. Unnecessary. Add axios by cdm and new function getupcomingfilms

--- JS/main.js
+++ JS/main.js
@@ -1,24 +1,36 @@
 //API for movies
 
-const renderFilms = films => {
-    for (const film of films){
-        document.querySelector('main.films').innerHTML+=`
+function renderFilms(films) {
+    for (const film of films) {
+        document.querySelector('main.films').innerHTML += `
         <div class="film">
             <h3>${film.title}</h3>
             <img src="https://image.tmdb.org/t/p/w185${film.poster_path}"
         </div>
-        `
+        `;
     }
 }
 
 
-const getPopularFilms = () => {
+const getPopularFilms = () => { //we will get the popular films using fetch
     fetch('https://api.themoviedb.org/3/movie/popular?api_key=079c21f9801a3caa498a95d9d9dfe4ca&language=es-ES')
     .then(res=>res.json())//json property parses the result 
     .then(res=>{
         const films = res.results;
-        console.log(films);
         renderFilms(films);
     })
     .catch(error=>console.error(error));
+}
+
+const getUpcomingFilms = async () => {
+    try {
+        const res = await axios.get('https://api.themoviedb.org/3/movie/upcoming?api_key=079c21f9801a3caa498a95d9d9dfe4ca&language=es-ES');
+    const films = res.data.results;
+    renderFilms(films);
+
+    } catch (error) {
+        console.error(error);
+    }
+    
+
 }
